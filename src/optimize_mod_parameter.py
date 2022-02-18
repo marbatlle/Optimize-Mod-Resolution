fb36c8981685ebc447de0cdea57de4af3fb95931
marbatlle <marbatlleperez@gmail.com>
2022-02-18 16:29:24 +0100

    update

--- src/optimize_mod_parameter.py
+++ src/optimize_mod_parameter.py
@@ -8,26 +8,24 @@ avg_size = pd.read_csv('output/output_avg_com_size.txt',header=None, names=["avg
 
 data = mod_param.merge(num_com, left_index=True, right_index=True)
 data = data.merge(avg_size, left_index=True, right_index=True)
+data['num_communities'] = data['num_communities'] - 1 
+export_data = data
+export_data = export_data.iloc[1: , :]
+export_data.to_csv('output/molti-output-analysis.txt', index=None, sep=' ', mode='a') 
+
+
 original_data = data
-
-data.to_csv('output/molti-output-analysis.txt', index=None, sep=' ', mode='a') 
-
 data = data[data.avg_community_size != 0]
-
 if data.empty:
     print("     error: Could not define an optimal value for this community structure", file=sys.stderr)
     exit()
     
 
 data_subset = data[['num_communities','avg_community_size']]
-
 data_diff = data_subset.diff()
 data_diff['result'] = data_diff['avg_community_size']/data_diff['num_communities']
-
 data_result = data_diff[['result']]
-
 data_mod_param = data[["mod_param"]]
-
 data_plot = data_mod_param.merge(data_result, left_index=True, right_index=True)
 data_plot = data_plot.iloc[2: , :]
 
@@ -37,16 +35,13 @@ data_plot = data_plot.dropna()
 data_plot['result'] = data_plot['result'].abs()
 
 data = data_plot.result.values.tolist()
-
 threshold = 0.01
-
 for i in range(len(data_plot)):
     if data_plot.result.iloc[i] < threshold:
         optimal_mod_param = data_plot.mod_param.iloc[i]
         break
     else:
         continue
-
 try:
     optimal_mod_param
 except NameError:
